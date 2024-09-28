a2455d70f4238c552963aa5b9351cdc54b98c2ca
stackgen-automation <automation@stackgen.com>
2024-09-28 10:33:17 +0000

    Automated commit

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,28 +1,4 @@
 
-output "aws_lambda_hello_kitty_function_vaishnavi25_function_url" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_url
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_function_vaishnavi25_invoke_arn" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.invoke_arn
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_function_vaishnavi25_function_arn" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_function_vaishnavi25_function_name" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_name
-  sensitive = false
-}
-
-
 output "aws_iam_role_hello_kitty_role_vaishnavi25_arn" {
   value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.arn
   sensitive = false
@@ -47,25 +23,49 @@ output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_name" {
 }
 
 
-output "aws_s3_vaishnavi25_arn" {
+output "aws_s3_vaishnavi_arn" {
   value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.arn
   sensitive = false
 }
 
 
-output "aws_s3_vaishnavi25_bucket_name" {
+output "aws_s3_vaishnavi_bucket_name" {
   value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.bucket_name
   sensitive = false
 }
 
 
-output "aws_s3_vaishnavi25_bucket_website_endpoint" {
+output "aws_s3_vaishnavi_bucket_website_endpoint" {
   value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.bucket_website_endpoint
   sensitive = false
 }
 
 
-output "aws_s3_vaishnavi25_kms_arn" {
+output "aws_s3_vaishnavi_kms_arn" {
   value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.kms_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_function_vaishnavi_function_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_function_vaishnavi_function_name" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_name
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_function_vaishnavi_function_url" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_url
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_function_vaishnavi_invoke_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.invoke_arn
   sensitive = false
 }
